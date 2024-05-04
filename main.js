e6cbfa91096c628b7190ad61a8bd5f51d36bb54d
Surendra2003 <surendra.kovela@gmail.com>
2024-05-04 22:09:12 +0530

    Adding portfolio to github

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",{
+    strings: ["Full stack Web Developer" , "Data Scientist" ,"Python Django Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
