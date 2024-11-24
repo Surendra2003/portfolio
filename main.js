23d39e17fff50f6027073a4c2eb5cefa7e6191f4
K.V.Surendra <96002022+Surendra2003@users.noreply.github.com>
2024-11-24 13:56:05 +0530

    Updated the skills and Projects

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 var typed = new Typed(".text",{
-    strings: ["Full stack Web Developer" , "Data Scientist" ,"Python Django Developer"],
+    strings: ["Software Developer" , "React Developer" ,"Python Developer","MERN Stack Developer","Power BI Developer","Data Scientist"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 });
 
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const element = document.querySelector('.send');
+//     if (element) {
+//         element.classList.add('new-class'); // Safely manipulate the DOM element
+//     } else {
+//         console.error('Element not found!');
+//     }
+// });
